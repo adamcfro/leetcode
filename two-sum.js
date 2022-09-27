ff8d184f1c0d614a7bd8e1150778a55566edebec
Adam Froehlich <adamcfro@gmail.com>
2022-09-27 10:17:33 -0400

    Two Sum description fixed

--- two-sum.js
+++ two-sum.js
@@ -1,9 +1,9 @@
 /**
-Given an array of integers nums and an integer target, return indices 
-of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you 
-may not use the same element twice.
+ * Given an array of integers, nums, and an integer, target, return indices
+ * of the two numbers such that they add up to target.
+ * 
+ * You may assume that each input would have exactly one solution, and you
+ * may not use the same element twice.
  * 
  * @param {Array} nums - An array of numbers
  * @param {number} target - A number two integers should sum to
