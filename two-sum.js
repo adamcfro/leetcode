cd86e03e7d47ec4cff424870997c331ff7293ecb
Adam Froehlich <adamcfro@gmail.com>
2022-09-27 10:13:48 -0400

    Cleaned up description

--- two-sum.js
+++ two-sum.js
@@ -1,16 +1,14 @@
 /**
- * Given an array of integers nums and an integer target, return indices 
- * of the two numbers such that they add up to target.
+Given an array of integers nums and an integer target, return indices 
+of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you 
 may not use the same element twice.
-
-You can return the answer in any order.
  * 
  * @param {Array} nums - An array of numbers
  * @param {number} target - A number two integers should sum to
  * @returns {Array} - Returns an array of indices of the two numbers that
- * add up to target
+ *    add up to target
  */
 function twoSum (nums, target) {
   for (let i = 0; i < nums.length; i++) {
