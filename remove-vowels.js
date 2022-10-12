e4c76bf8a697dfccc6bc75692c6edd605ae3bc63
Adam Froehlich <adamcfro@gmail.com>
2022-10-12 15:17:09 -0400

    Solution for LeetCode problem #1119

---
+++ remove-vowels.js
@@ -0,0 +1,10 @@
+/**
+ * Given a string s, removes the vowels 'a', 'e', 'i', 'o', and 'u' from it.
+ * Returns the modified string.
+ * 
+ * @param {string} s - A string parameter
+ * @returns {string} - Returns a modified string with all vowels removed
+ */
+function removeVowels (s) {
+  return s.replace(/[aeiou]/gi, '');
+}
