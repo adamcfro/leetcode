5ce7f846f71b1ecffbceebcb4f6e530dbf8fa968
Adam Froehlich <adamcfro@gmail.com>
2022-10-15 14:31:38 -0400

    feat(solution): LeetCode problem #2235

---
+++ add-two-integers.js
@@ -0,0 +1,10 @@
+/**
+ * Given two integers num1 and num2, returns the sum of the two integers.
+ * 
+ * @param {number} num1 - First number parameter 
+ * @param {number} num2 - Second number parameter
+ * @returns {number} - Returns the sum of two numbers
+ */
+function sum (num1, num2) {
+  return num1 + num2;
+}
