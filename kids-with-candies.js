3b3217f97e82f61add47fdcf9cf21911cdca6179
Adam Froehlich <adamcfro@gmail.com>
2022-10-19 15:26:03 -0400

    feat(solution): LeetCode problem #1431

---
+++ kids-with-candies.js
@@ -0,0 +1,23 @@
+/**
+ * Given n kids and an integer array candies, returns an array of booleans 
+ * stating if when extraCandies is added to candies[i], the ith kid now has
+ * the most candies.
+ * 
+ * Note that multiple kids can have the greatest number of candies.
+ * 
+ * @param {number[]} candies - An array of numbers
+ * @param {number} extraCandies - A number parameter
+ * @returns {boolean[]} - Returns an array of booleans
+ */
+function kidsWithCandies (candies, extraCandies) {
+  let maxCandy = Math.max(...candies);
+  let hasGreatest = [];
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] + extraCandies >= maxCandy) {
+      hasGreatest.push(true);
+    } else {
+      hasGreatest.push(false);
+    }
+  }
+  return hasGreatest;
+}
