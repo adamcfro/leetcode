be588c0c7073f04b42f29056fc901c4ea8a999b3
Adam Froehlich <adamcfro@gmail.com>
2022-10-22 16:28:58 -0400

    feat(solution): add LeetCode problem #2006

---
+++ count-k-difference.js
@@ -0,0 +1,19 @@
+/**
+ * Given an integer array nums and an integer k, returns the number of pairs
+ * whose absolute value is equal to k.
+ * 
+ * @param {number[]} nums - An array of numbers
+ * @param {number} k - A target number
+ * @returns {number} - Returns a count of pairs whose absolute value equals k
+ */
+function countKDifference (nums, k) {
+  let count = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (Math.abs(nums[i] - nums[j]) === k) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
