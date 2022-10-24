aa1e8e862810c58f15d164c8c48f2052ff415363
Adam Froehlich <adamcfro@gmail.com>
2022-10-24 10:01:49 -0400

    feat(solution): add LeetCode problem #2176

---
+++ count-equal-and-divisible.js
@@ -0,0 +1,12 @@
+
+function countPairs (nums, k) {
+  let count = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j] && (i * j) % k === 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
