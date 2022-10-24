91b8682114abce61276e2641bcbb90036a64a58f
Adam Froehlich <adamcfro@gmail.com>
2022-10-24 10:09:29 -0400

    docs(count-equal-and-divisible.js): add documentation

--- count-equal-and-divisible.js
+++ count-equal-and-divisible.js
@@ -1,4 +1,12 @@
-
+/**
+ * Given a 0-indexed integer array nums and an integer k,
+ * returns the number of pairs where nums[i] === nums[j]
+ * and (i * j) is divisible by k.
+ * 
+ * @param {number[]} nums - An array of numbers
+ * @param {number} k - A number used for division
+ * @returns {number} - Returns a count number
+ */
 function countPairs (nums, k) {
   let count = 0;
   for (let i = 0; i < nums.length - 1; i++) {
