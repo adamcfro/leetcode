0820f78e4fd92d0f1eb792527b123ec5927777c9
Adam Froehlich <adamcfro@gmail.com>
2022-10-30 09:11:44 -0400

    feat(solution): add LeetCode problem #1085

---
+++ sum-of-digits.js
@@ -0,0 +1,16 @@
+/**
+ * Given an integer array nums, returns 0 if the sum of the digits of the
+ * minimum integer in nums is odd, or 1 otherwise.
+ * 
+ * @param {number[]} nums - An array of numbers
+ * @returns {number} - Returns 0 or 1
+ */
+function sumOfDigits (nums) {
+  let sum = 0;
+  let min = Math.min(...nums);
+  while (min > 0) {
+    sum += min % 10;
+    min = Math.floor(min / 10);
+  }
+  return (sum + 1) % 2;
+}
