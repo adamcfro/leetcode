6c0864d49832d07d2e797843bc4d2dcdc0f96643
Adam Froehlich <adamcfro@gmail.com>
2022-11-01 10:41:28 -0400

    feat(solution): add LeetCode problem #1475

---
+++ final-prices.js
@@ -0,0 +1,21 @@
+/**
+ * Given an integer array prices where prices[i] is the price of the ith item,
+ * returns the array after considering the discounts the shop provides.
+ * 
+ * A discount is applied to prices[i] when prices[j] is the minimum index
+ * after i where prices[j] <= prices[i].
+ * 
+ * @param {number[]} prices 
+ * @returns {number[]}
+ */
+function finalPrices (prices) {
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      if (prices[j] <= prices[i]) {
+        prices[i] -= prices[j];
+        break;
+      }
+    }
+  }
+  return prices;
+}
