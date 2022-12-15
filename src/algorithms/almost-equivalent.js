69ae6a034024ed02576da5af79cc7e26fa02bf5c
Adam Froehlich <adamcfro@gmail.com>
2022-12-15 10:10:13 -0500

    feat(solution): add LeetCode problem #2068

---
+++ src/algorithms/almost-equivalent.js
@@ -0,0 +1,27 @@
+/**
+ * Two strings are considered almost equivalent if the difference between the
+ * frequencies of each letter from 'a' to 'z' is at most 3. The frequency of
+ * a letter x is the number of times it occurs in the string.
+ * 
+ * Given two strings each of length n, returns true if word1 and word2 are
+ * almost equivalent, or false otherwise.
+ * 
+ * @param {string} word1 - First string of English characters
+ * @param {string} word2 - Second string of English characters
+ * @returns {boolean} - Returns true if the two strings are almost equivalent
+ */
+function checkAlmostEquivalent (word1, word2) {
+  let frequency = {};
+  for (const char of word1) {
+    frequency[char] = (frequency[char] || 0) + 1;
+  }
+  for (const char of word2) {
+    frequency[char] = (frequency[char] || 0) - 1;
+  }
+  for (const [key, val] of Object.entries(frequency)) {
+    if (frequency[key] > 3 || frequency[key] < -3) {
+      return false;
+    }
+  }
+  return true;
+}
