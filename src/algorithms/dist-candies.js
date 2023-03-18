f7002ec1ccff15fcc175d62f88d99a3f68c474e1
Adam Froehlich <adamcfro@gmail.com>
2023-03-18 11:51:01 -0400

    feat(solution): add LeetCode problem #575

---
+++ src/algorithms/dist-candies.js
@@ -0,0 +1,19 @@
+/**
+ * Alice has n candies, where the ith candy is of type candyType[i]. Alice 
+ * noticed that she started to gain weight, so she visited a doctor.
+ * 
+ * The doctor advised Alice to only eat n / 2 of the candies she has (n is 
+ * always even). Alice likes her candies very much, and she wants to eat the 
+ * maximum number of different types of candies while still following the 
+ * doctor's advice.
+ * 
+ * Given the integer array candyType of length n, return the maximum number of 
+ * different types of candies she can eat if she only eats n / 2 of them.
+ * 
+ * @param {number[]} candyType - Numbers denoting types of candy
+ * @returns {number} - Returns the max number of different candy to be eaten
+ */
+function distributeCandies (candyType) {
+  let unique = new Set(candyType);
+  return Math.min(unique.size, Math.floor(candyType.length / 2));
+}
