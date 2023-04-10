ad6375ab23b1d37827f1bd4f22a6506c1cd777bb
Adam Froehlich <adamcfro@gmail.com>
2023-04-10 09:57:16 -0400

    refactor(solution): change return parameter problem #20

--- src/algorithms/valid-parentheses.js
+++ src/algorithms/valid-parentheses.js
@@ -1,6 +1,6 @@
 /**
  * Given a string s containing just the characters 
- * '(', ')', '{', '}', '[', and ']', determine if the input string is valid.
+ * '(', ')', '{', '}', '[', and ']', determines if the input string is valid.
  * 
  * An input string is valid if:
  *  1. Open brackets must be closed by the same type of brackets.
@@ -24,5 +24,5 @@ function validParentheses (s) {
       }
     }
   }
-  return stack.length <= 0;
+  return stack.length === 0;
 }
