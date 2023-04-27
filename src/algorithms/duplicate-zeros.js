42de708c3f3e7a56e6358fc5755986167293d4b1
Adam Froehlich <adamcfro@gmail.com>
2023-04-27 10:14:13 -0400

    feat(solution): add LeetCode problem #1089

---
+++ src/algorithms/duplicate-zeros.js
@@ -0,0 +1,17 @@
+/**
+ * Given a fixed-length integer array arr, duplicates each occurrence of zero,
+ * shifting the remaining elements to the right. Returns the modified array.
+ * 
+ * @param {number[]} arr - An array of numbers
+ * @returns {number[]} - Returns a modified array of numbers
+ */
+function duplicateZeros (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      arr.pop();
+      arr.splice(i + 1, 0, 0);
+      i++
+    }
+  }
+  return arr;
+}
