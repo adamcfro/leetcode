7fdfd7bbc63138bdf2c690dfd9f05bed673b3d54
Adam Froehlich <adamcfro@gmail.com>
2023-05-13 09:45:57 -0400

    refactor(solution): fix parameter descriptions problem #1431

--- src/algorithms/kids-with-candies.js
+++ src/algorithms/kids-with-candies.js
@@ -5,8 +5,10 @@
  * 
  * Note that multiple kids can have the greatest number of candies.
  * 
- * @param {number[]} candies - An array of numbers
- * @param {number} extraCandies - A number parameter
+ * @param {number[]} candies - An array of numbers representing the candies
+ *     each kid has
+ * @param {number} extraCandies - A number parameter representing the extra
+ *     candies that are available
  * @returns {boolean[]} - Returns an array of booleans
  */
 function kidsWithCandies (candies, extraCandies) {
