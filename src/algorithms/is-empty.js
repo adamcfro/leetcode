cad2d65db2a67059278d9274e11b7a6e9af26f50
Adam Froehlich <adamcfro@gmail.com>
2023-06-13 09:37:01 -0400

    feat(solution): add LeetCode problem #2727

---
+++ src/algorithms/is-empty.js
@@ -0,0 +1,12 @@
+/**
+ * Given an object or an array, returns true if it is empty.
+ * 
+ * @param {Object | Array} obj - Object/Array that is the output of JSON.parse
+ * @returns {boolean} - Returns true if the parameter is empty, false otherwise
+ */
+function isEmpty (obj) {
+  if (Object.keys(obj).length > 0 || obj.length > 0) {
+    return false;
+  }
+  return true;
+}
