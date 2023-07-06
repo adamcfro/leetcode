eed71bae4cc5a122b99835110a2a235d2052161f
Adam Froehlich <adamcfro@gmail.com>
2023-07-06 09:54:28 -0400

    refactor(solution): add parameter and function descriptions problem #88

--- src/algorithms/merge-sorted-array.js
+++ src/algorithms/merge-sorted-array.js
@@ -1,4 +1,15 @@
-
+/**
+ * This function takes two integer arrays nums1 and nums2, sorted in 
+ * non-decreasing order, and two integers m and n, representing the number of 
+ * elements in nums1 and nums2 respectively, and merges nums1 and nums2 into a 
+ * single array sorted in non-decreasing order. Returns the modified array.
+ * 
+ * @param {number[]} nums1 - An array of numbers
+ * @param {number} m - The length of the nums1 array
+ * @param {number[]} nums2 - An array of numbers
+ * @param {number} n - The length of the nums2 array
+ * @returns {number[]} - Returns nums1 after it has been modified
+ */
 function mergeSortedArray (nums1, m, nums2, n) {
   let i = m - 1;
   let j = n - 1;
