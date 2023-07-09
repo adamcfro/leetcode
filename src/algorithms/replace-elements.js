058acec090dddbc463eb2656e52297361c683a6c
Adam Froehlich <adamcfro@gmail.com>
2023-07-09 09:06:56 -0400

    refactor(solution): fix description and parameter names problem #1299

--- src/algorithms/replace-elements.js
+++ src/algorithms/replace-elements.js
@@ -1,9 +1,7 @@
 /**
- * Given an array arr, replaces every element in that array with the greatest
+ * Given an array arr, replaces every element in the array with the greatest
  * element among the elements to its right, and replaces the last element
- * with -1.
- * 
- * Returns the modified array.
+ * with -1. Returns the modified array.
  * 
  * @param {number[]} arr - An array of numbers
  * @returns {number[]} - Returns a modified array of numbers
@@ -12,9 +10,9 @@ function replaceElements (arr) {
   let greatestElem = -1;
   for (let i = arr.length - 1; i >= 0; i--) {
     if (arr[i] > greatestElem) {
-      let temp = arr[i]
+      let current = arr[i]
       arr[i] = greatestElem;
-      greatestElem = temp;
+      greatestElem = current;
     } else {
       arr[i] = greatestElem;
     }
