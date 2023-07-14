095255362c41b6375f92030269c4fb5b6cf3d223
Adam Froehlich <adamcfro@gmail.com>
2023-07-14 10:16:02 -0400

    feat(solution): add LeetCode problem #1119

--- src/algorithms/remove-vowels.js
+++ src/algorithms/remove-vowels.js
@@ -2,7 +2,7 @@
  * Given a string s, removes the vowels 'a', 'e', 'i', 'o', and 'u' from it.
  * Returns the modified string.
  * 
- * @param {string} s - A string parameter
+ * @param {string} s - A string consisting of lowercase English letters
  * @returns {string} - Returns a modified string with all vowels removed
  */
 function removeVowels (s) {
