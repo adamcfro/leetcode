ec14259a81103c4836e861ce47503f5ac8f7cfba
Adam Froehlich <adamcfro@gmail.com>
2023-09-20 16:19:58 -0700

    feat(solution): add LeetCode problem #189

---
+++ src/algorithms/rotate-array.js
@@ -0,0 +1,18 @@
+/**
+ * Given an integer array nums, rotate the array to the right by k steps,
+ * where k is non-negative.
+ * 
+ * @param {number[]} nums - An array of numbers
+ * @param {number} k - A non-negative number
+ * @returns {number[]} - Returns a rotated array
+ */
+function rotateArray (nums, k) {
+  let arr = [];
+  for (let i = 0; i < nums.length; i++) {
+    arr[(i + k) % nums.length] = nums[i];
+  }
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = arr[i];
+  }
+  return;
+}
