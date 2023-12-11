33663fda05112dea92c03a1e1cbb5c70c542d92b
Adam Froehlich <adamcfro@gmail.com>
2023-12-11 11:35:22 -0800

    feat(solution): add LeetCode problem #2960

---
+++ src/algorithms/count-tested-devices.js
@@ -0,0 +1,32 @@
+/**
+ * You are given a 0-indexed integer array batteryPercentages having length n,
+ * denoting the battery percentages of n 0-indexed devices. Your task is to
+ * test each device i in order from 0 to n - 1, by performing the following
+ * test operations:
+ *   If batteryPercentages[i] is greater than 0:
+ *     • Increment the count of tested devices.
+ *     • Decrease the battery percentage of all devices with indices j in the
+ *       range [i + 1, n - 1] by 1, ensuring their percentage never goes below
+ *       0, i.e, batteryPercentages[j] = max(0, batteryPercentages[j] - 1).
+ *     • Move to the next device.
+ *   Otherwise, move to the next device without performing any test.
+ *
+ * Returns an integer denoting the number of devices that will be tested after
+ * performing the test operations in order.
+ *
+ * @format
+ * @param {number[]} batteryPercentages - An array of number representing battery percentages
+ * @returns {number} - Returns a count of devices with a battery percentage greater than 0
+ */
+
+function countTestedDevices(batteryPercentages) {
+  let testedDevices = 0;
+  let increment = 0;
+  batteryPercentages.forEach((element) => {
+    if (element - increment > 0) {
+      testedDevices++;
+      increment++;
+    }
+  });
+  return testedDevices;
+}
