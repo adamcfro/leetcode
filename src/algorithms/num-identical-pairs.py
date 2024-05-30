24fdc6cb5feb442f9f603f8563fe043b849b21d1
Adam Froehlich <adamcfro@gmail.com>
2024-05-30 08:52:34 -0700

    feat(solution): add Python solution problem #1512

---
+++ src/algorithms/num-identical-pairs.py
@@ -0,0 +1,18 @@
+def num_identical_pairs(nums):
+    """Given an array of integers nums, return the number of good pairs. A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+    Args:
+        nums (List[int]): A list of integers
+
+    Returns:
+        Integer: Returns a count of good pairs
+    """
+    count = 0
+    elems = {}
+    for num in nums:
+        if num in elems:
+            count += elems[num]
+            elems[num] += 1
+        else:
+            elems[num] = 1
+    return count
