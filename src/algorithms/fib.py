d599b0eb65267d0f74a7e1bc4c35c82b590c1387
Adam Froehlich <adamcfro@gmail.com>
2024-05-31 09:18:49 -0700

    feat(solution): add Python solution problem #509

---
+++ src/algorithms/fib.py
@@ -0,0 +1,23 @@
+def fib(n):
+    """The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is:
+    • F(0) = 0, F(1) = 1
+    • F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+    Given n, calculate F(n).
+
+    Args:
+        n (integer): An integer representing the nth Fibonacci number
+
+    Returns:
+        integer: Returns the nth Fibonacci number
+    """
+    f1 = 0
+    f2 = 1
+    curr = 0
+    if (n <= 1):
+        return n
+    for i in range(2, n + 1):
+        curr = f1 + f2
+        f1 = f2
+        f2 = curr
+    return curr
