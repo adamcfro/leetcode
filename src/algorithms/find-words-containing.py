5271f89d8984dac12edd8204a99a3d7e919ca30b
Adam Froehlich <adamcfro@gmail.com>
2024-06-02 11:24:43 -0700

    feat(solution): add Python solution problem #2942

---
+++ src/algorithms/find-words-containing.py
@@ -0,0 +1,19 @@
+def find_is_containing(words, x):
+    """You are given a 0-indexed array of strings words and a character x.Return an array of indices representing the words that contain the character x. Note that the returned array may be in any order.
+
+    Args:
+        words (List[str]): A list of strings
+        x (String): A lowercase English letter
+
+    Returns:
+        List[int]: Returns a list of indices
+    """
+    res = []
+    index = 0
+    for word in words:
+        for char in word:
+            if char == x:
+                res.append(index)
+                break
+        index += 1
+    return res
