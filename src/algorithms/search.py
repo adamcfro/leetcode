173ac22a9f7cde236f657e65e37c1cefda33fbee
Adam Froehlich <adamcfro@gmail.com>
2024-06-03 09:42:28 -0700

    feat(solution): add Python solution problem #704

---
+++ src/algorithms/search.py
@@ -0,0 +1,21 @@
+def search(nums, target):
+    """Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+    Args:
+        nums (List[int]): A list of integers
+        target (Integer): A target integer
+
+    Returns:
+        Integer: Returns the target index if it exists, else returns -1
+    """
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
