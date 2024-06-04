f3b4be1c8ee0ab5ad17b68dd25fae6cfed7cfd78
Adam Froehlich <adamcfro@gmail.com>
2024-06-04 10:05:42 -0700

    feat(solution): add Python solution problem #1207

---
+++ src/algorithms/unique-occurrences.py
@@ -0,0 +1,18 @@
+def unique_occurrences(arr):
+    """Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+    Args:
+        arr (List[int]): A list of integers
+
+    Returns:
+        Boolean: Returns true if the number of occurrences of each value in the array is unique or false otherwise
+    """
+    elems = {}
+    seen_vals = set()
+    for num in arr:
+        elems[num] = elems.get(num, 0) + 1
+    for val in elems.values():
+        if val in seen_vals:
+            return False
+        seen_vals.add(val)
+    return True
