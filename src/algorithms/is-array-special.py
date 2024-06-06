f771959ebbb02374e4ded43e8cd30b1abc43801c
Adam Froehlich <adamcfro@gmail.com>
2024-06-06 08:47:04 -0700

    feat(solution): add Python solution problem #3151

---
+++ src/algorithms/is-array-special.py
@@ -0,0 +1,17 @@
+def is_array_special(nums):
+    """An array is considered special if every pair of its adjacent elements contains two numbers with different parity. You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
+
+    Args:
+        nums (List[int]): A list of positive integers
+
+    Returns:
+        Boolean: Returns true if all adjacent elements in nums have different parity
+    """
+    if len(nums) == 1:
+        return True
+    for i in range(len(nums) - 1):
+        if (nums[i] % 2 == 0 and nums[i + 1] % 2 == 0) or (
+            nums[i] % 2 == 1 and nums[i + 1] % 2 == 1
+        ):
+            return False
+    return True
