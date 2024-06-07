cefeb84c7a601796c3c62035a338ddb83da6b7ce
Adam Froehlich <adamcfro@gmail.com>
2024-06-07 10:43:48 -0700

    feat(solution): add Python solution problem #1446

---
+++ src/algorithms/max-power.py
@@ -0,0 +1,24 @@
+def max_power(s):
+    """A function to determine the power of a string.
+
+    The power of the string is the maximum length of a non-empty substring that contains only one unique character. Given a string s, return the power of s.
+
+    Args:
+        s (String): A string consisting of lowercase English letters.
+
+    Returns:
+        Integer: Returns an integer denoting the power of s.
+    """
+    max_count = 1
+    temp_count = 1
+    length = len(s)
+    if length == 1:
+        return length
+    for i in range(1, length):
+        if s[i] == s[i - 1]:
+            temp_count += 1
+            max_count = max(temp_count, max_count)
+        else:
+            max_count = max(temp_count, max_count)
+            temp_count = 1
+    return max_count
