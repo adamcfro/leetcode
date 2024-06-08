f35221bd162a5a8ede55b21ffc7fb6404e50bd72
Adam Froehlich <adamcfro@gmail.com>
2024-06-08 09:46:57 -0700

    feat(solution): add Python solution problem #1662

---
+++ src/algorithms/array-strings-are-equal.py
@@ -0,0 +1,13 @@
+def array_strings_are_equal(word1, word2):
+    """Tests if two lists are equivalent after concatenation.
+
+    A string is represented by an array if the array elements concatenated in order forms the string. Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+    Args:
+        word1 (List[str]): A list of lowercase English strings.
+        word2 (List[str]): A list of lowercase English strings.
+
+    Returns:
+        Boolean: Returns true if two lists are equivalent after concatenation.
+    """
+    return "".join(word1) == "".join(word2)
