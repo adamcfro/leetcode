de84f373508c3d41b166874e6e6d69efb4e1e43e
Adam Froehlich <adamcfro@gmail.com>
2024-06-09 11:32:42 -0700

    feat(solution): add Python solution problem #709

---
+++ src/algorithms/to-lower-case.py
@@ -0,0 +1,10 @@
+def to_lower_case(s):
+    """Returns a string after replacing every uppercase letter with the same lowercase letter.
+
+    Args:
+        s (String): A string of printable ASCII characters.
+
+    Returns:
+        String: Returns a string of all lowercase characters.
+    """
+    return s.lower()
