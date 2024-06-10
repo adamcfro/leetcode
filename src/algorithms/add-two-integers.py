dd033589ea09d824e3fe20940bd4dda95eb33de1
Adam Froehlich <adamcfro@gmail.com>
2024-06-10 09:37:10 -0700

    feat(solution): add Python solution problem #2235

---
+++ src/algorithms/add-two-integers.py
@@ -0,0 +1,11 @@
+def add_two_integers(num1, num2):
+    """Given two integers num1 and num2, return the sum of the two integers.
+
+    Args:
+        num1 (Integer): An integer.
+        num2 (Integer): An integer.
+
+    Returns:
+        Integer: Returns the sum of num1 and num2.
+    """
+    return num1 + num2
