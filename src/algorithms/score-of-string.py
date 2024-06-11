d7eb3a58e9de6e4d4f9c9fd52ba72fe44dba61a4
Adam Froehlich <adamcfro@gmail.com>
2024-06-11 09:11:03 -0700

    feat(solution): add Python solution problem #3110

---
+++ src/algorithms/score-of-string.py
@@ -0,0 +1,15 @@
+def score_of_string(s):
+    """Calculates and returns the score of a string.
+
+    The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters. Returns the score of s.
+
+    Args:
+        s (String): A string of lowercase English letters.
+
+    Returns:
+        Integer: Returns the score of a string as an integer.
+    """
+    score = 0
+    for i in range(len(s) - 1):
+        score += abs(ord(s[i]) - ord(s[i + 1]))
+    return score
