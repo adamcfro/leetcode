a170357e60c15e0575071fc66fff6e457fb69446
Adam Froehlich <adamcfro@gmail.com>
2024-07-25 09:07:29 -0700

    feat(solution): add Python solution problem #3019

---
+++ src/algorithms/count-key-changes.py
@@ -0,0 +1,21 @@
+def count_key_changes(s):
+    """You are given a 0-indexed string s typed by a user. Changing a key is 
+    defined as using a key different from the last used key. For example, s = 
+    "ab" has a change of a key while s = "bBBb" does not have any. Return the 
+    number of times the user had to change the key.
+
+    Note: Modifiers like shift or caps lock won't be counted in changing the 
+    key that is if a user typed the letter 'a' and then the letter 'A' then it 
+    will not be considered as a changing of key.
+
+    Args:
+        s (str): A string of English letters.
+
+    Returns:
+        int: Returns a count of key changes.
+    """
+    count = 0
+    for i in range(1, len(s)):
+        if s[i - 1].upper() != s[i].upper():
+            count += 1
+    return count
